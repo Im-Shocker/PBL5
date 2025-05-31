1fcefdb9a173052548b7597f8bba4699d81f7e0a
Im-Shocker <giacatdu1412@gmail.com>
2025-05-31 21:46:47 +0700

    update XML

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,10 @@ android {
 }
 
 dependencies {
-
+    implementation ("androidx.camera:camera-camera2:1.2.3")
+    implementation ("androidx.camera:camera-lifecycle:1.2.3")
+    implementation ("androidx.camera:camera-view:1.2.3")
+    implementation("androidx.activity:activity:1.6.1")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
